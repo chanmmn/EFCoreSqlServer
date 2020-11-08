d96df080eb61b845a1e1d3b1adebd7fe6df8a1c4
Ming Man Chan <chanmmn@hotmail.com>
2020-11-08 20:51:22 +0800

    Commit Entity Framework Core for SQL Server
    Commit Entity Framework Core for SQL Server

---
+++ ConAppEF/ConAppEF/Model.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public string conn = "data source=.;initial catalog=blogging;integrated security=True";
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            //=> options.UseSqlite("Data Source=blogging.db");
+            => options.UseSqlServer(conn);
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
---
+++ ConAppEF/ConAppEF/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var db = new BloggingContext())
+            {
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog {BlogId=1, Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                //blog.Url = "https://devblogs.microsoft.com/dotnet";
+                //blog.Posts.Add(
+                //    new Post
+                //    {
+                //        PostId = 1,
+                //        Title = "Hello World",
+                //        Content = "I wrote an app using EF Core!",
+                //        BlogId = 1
+                //    }); ;
+                db.Add(new Post
+                {
+                    PostId = 1,
+                    Title = "Hello World",
+                    Content = "I wrote an app using EF Core!",
+                    BlogId = 1
+                }
+                    );
+                db.SaveChanges();
+
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
+        }
+    }
+}
