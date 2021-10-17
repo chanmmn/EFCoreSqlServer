45ef1861c22950b152c82a1cbf3a815b3f7bca00
chanmmn <33271651+chanmmn@users.noreply.github.com>
2021-10-17 12:54:07 +0800

    Commit Lazy Loading
    Commit Lazy Loaading

---
+++ SampleEF/SampleEF/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LazyLoading();
+        }
+
+        public static void LazyLoading()
+        {
+            using (SampleEntities1 dbContext = new SampleEntities1())
+            {
+                //dbContext.Log = Console.Out;
+
+                foreach (Department dept in dbContext.Departments)
+                {
+                    Console.WriteLine(dept.Name);
+                    foreach (Employee emp in dept.Employees)
+                    {
+                        Console.WriteLine("\t" + emp.FirstName + " " + emp.LastName);
+                    }
+                }
+            }
+        }
+
+        //public void EagerLoading()
+        //{
+        //    using (SampleEntities1 dbContext = new SampleEntities1())
+        //    {
+        //        //dbContext.Log = Console.Out;
+
+        //        // Load related Employee entities along with the Department entity
+        //        DataLoadOptions loadOptions = new DataLoadOptions();
+        //        loadOptions.LoadWith<Department>(d => d.Employees);
+        //        dbContext.LoadOptions = loadOptions;
+
+        //        foreach (Department dept in dbContext.Departments)
+        //        {
+        //            Console.WriteLine(dept.Name);
+        //            foreach (Employee emp in dept.Employees)
+        //            {
+        //                Console.WriteLine("\t" + emp.FirstName + " " + emp.LastName);
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
